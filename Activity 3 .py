06d0282da913ff751e98f8e5031bf51d45b25e91
DEGORIO18 <andriedegorio6@gmail.com>
2026-08-29 15:25:29 +0800

    Add files via upload

---
+++ Activity 3 .py
@@ -0,0 +1,15 @@
+ # integers variable
+age = 18
+# float variable 
+price = 18.91 
+# string 
+name = "degorio andrie"
+
+#boolean
+handsome = True
+
+#print
+print("my name ",name)
+print("and rigth now my age is ", age)
+print("the price is ",price)
+
---
+++ Activity 4.py
@@ -0,0 +1,11 @@
+# escape sequence 
+# a way to modify inline string 
+# \n - new line
+# \t - tab space 
+# \r - carriage return 
+# \\ - insert slash
+# \" - insert quotation
+# \b - back space 
+emo_phase = "The quick brown fox \rjums \\over the layz dog. \n \t\t\"To fall in love takes only a second, to let go takes\b\b\b\b time awst"
+print(emo_phase)
+
