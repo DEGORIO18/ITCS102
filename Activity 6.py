8623761eb4fba0bd21705bd89134df7a3998a075
DEGORIO18 <andriedegorio6@gmail.com>
2026-09-03 15:04:34 +0800

    Add files via upload

---
+++ Activity 6.py
@@ -0,0 +1,13 @@
+name = "Degorio Andrie"
+#first step to dynamic content is INPUTS (user input)
+print("Hi", name,"how are you doin ? ")
+#code is not dynamic - ever changing, static - fixed
+
+
+#int(), or eval (), float()
+x = int(input("What is your name -- > ? "))
+
+sum = x + 4 
+
+print("total = ", sum)
+print(type(x))
