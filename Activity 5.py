2cdd95e888e1fd7dcf8732679e3550c040ee3515
DEGORIO18 <andriedegorio6@gmail.com>
2026-09-03 11:23:37 +0800

    Add files via upload

---
+++ Activity 5.py
@@ -0,0 +1,16 @@
+#operators
+#Arithmeric
+# + - subraction 
+# * - Multipication 
+# / Divison
+# // Floor Division 
+# ** Exponentation 
+# % Modulus
+
+a = 55
+b = 5
+sum = a + b
+diff = a - b 
+product = a * b 
+division = a / b 
+print ("The sum of ",a," and ",b, ",is", sum)
