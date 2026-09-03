00212af6341e40b46756700fb6e6371030be98a0
DEGORIO18 <andriedegorio6@gmail.com>
2026-09-03 15:41:20 +0800

    Add files via upload

---
+++ activity 7.py
@@ -0,0 +1,16 @@
+total = 0
+
+n1 = eval(input("input a number ---> "))
+total = total + n1
+
+n2 = eval(input("input a number ---> "))
+total += n2
+
+n3 = eval(input("input a number ---> "))
+total += n3
+
+print("total is ---> ", total)
+
+
+
+
